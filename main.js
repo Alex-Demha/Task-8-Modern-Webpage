9792ceab2d18ff87c6eb05ad86a564986c053ea8
Alex-Demha <79983650+Alex-Demha@users.noreply.github.com>
2021-05-13 17:39:39 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ $btn.on("click", function(){
     $("nav").toggleClass("reveal");
     $("button").toggleClass("close");
     $(".wrapper").toggleClass('dimBody');
-    $(".clip1").toggleClass('dim');
-    $(".clip2").toggleClass('dim');
-    $(".diamond").toggleClass('dim');
+    $(".clip1").toggleClass('dim1');
+    $(".clip2").toggleClass('dim2');
     $(".text2").toggleClass('dimSVG');
 });
